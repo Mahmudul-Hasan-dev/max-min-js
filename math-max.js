d40937ee616b8c7f186d52f02022ba646258d143
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-09 08:36:42 +0600

    math-max done

---
+++ math-max.js
@@ -0,0 +1,13 @@
+//find max from array
+var arr = [10, 12, 13];
+//to find from array we have to use three dot in math.max function
+var max = Math.max(...arr);
+console.log(max);
+
+//from genral number
+var num1 = 10;
+var num2 = 12;
+var num3 = 14;
+//we can call three variable in math.max function
+var max2 = Math.max(num1, num2, num3);
+console.log(max2);
