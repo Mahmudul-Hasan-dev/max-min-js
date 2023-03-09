b2ec3002f9a0e13add9f478ca87746f798f9c2fc
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-09 08:40:46 +0600

    math-min done

---
+++ math-min.js
@@ -0,0 +1,16 @@
+var arr = [34, 32, 35];
+
+//we have to put three dot to get access of array
+
+var min = Math.min(...arr);
+console.log(min);
+
+//from general number
+var num1 = 2;
+var num2 = 1;
+var num3 = 8;
+
+//math.min function use tofind minimum from given parameter
+
+var minimum = Math.min(num1, num2, num3);
+console.log(minimum);
