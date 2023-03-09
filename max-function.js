08297f898dfaa43af3d1cf7bb74961fb28cca973
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-09 08:26:45 +0600

    max-min done

--- max-function.js
+++ max-function.js
@@ -1,7 +1,13 @@
+//declare function to find maximum of three number and take parameter from call function
 function findMax(num1, num2, num3) {
+
+    //declare a variable to store the initial max number from condition
     var largeNumber;
+
+    //select one and compare with others
     if (num1 > num2 && num1 > num3) {
         console.log('the largest is:');
+        //store as max if condition is true
         largeNumber = num1;
     }
     else if (num2 > num1 && num2 > num3) {
@@ -12,11 +18,15 @@ function findMax(num1, num2, num3) {
         console.log('the largest is');
         largeNumber = num3;
     }
+    //return the stored max
     return largeNumber;
 }
 
+//declare variable
 var number1 = 100;
 var number2 = 17;
 var number3 = 12;
+
+//call function and send variable as parameter of call function
 var max = findMax(number1, number2, number3);
 console.log(max);
--- min-function.js
+++ min-function.js
@@ -1,7 +1,30 @@
+//declare function to find minimum of three number and take parameter from call function
 function findMin(num1, num2, num3) {
+    //declare variable to store initial minimum values
     var smallestNumber;
+
+    //select one and compare with others
     if (num1 < num2 && num1 < num3) {
         console.log('smallest number is:');
+        //store the inital minimum in the variable
         smallestNumber = num1;
     }
+    else if (num2 < num1 && num2 < num3) {
+        console.log('smallest number is:');
+        smallestNumber = num2;
+    }
+    else {
+        console.log('smallest number is:');
+        smallestNumber = num3;
+    }
+    //return the minimum stored value
+    return smallestNumber;
 }
+
+var number1 = 10;
+var number2 = 12;
+var number3 = 5;
+
+//call function and send variable as parameter
+var minimum = findMin(number1, number2, number3);
+console.log(minimum);
